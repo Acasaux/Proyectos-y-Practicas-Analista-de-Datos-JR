d3434cf51518f44c9727615563a2380b84fd6657
Agustin Casaux <95194455+CasauxAgustin@users.noreply.github.com>
2025-11-13 21:29:06 -0300

    Create Crear_Datos_1.sql

---
+++ Scrips SQL/Crear_Datos_1.sql
@@ -0,0 +1,227 @@
+INSERT INTO accesorios_prime (efimero, syandana, companiero) VALUES
+-- 1 Excalibur Prime
+('Ninguno', 'Pendragon Prime Syandana', 'Ninguno'),
+-- 2 Frost Prime
+('Ninguno', 'Misa Prime Syandana', 'Ninguno'),
+-- 3 Mag Prime
+('Ninguno', 'Verlorum Prime Syandana', 'Ninguno'),
+-- 4 Ember Prime
+('Ninguno', 'Pyra Prime Syandana', 'Ninguno'),
+-- 5 Rhino Prime
+('Ninguno', 'Noble Prime Syandana', 'Ninguno'),
+-- 6 Loki Prime
+('Ninguno', 'Asa Prime Syandana', 'Ninguno'),
+-- 7 Nova Prime
+('Ninguno', 'Verlorum Prime Syandana', 'Ninguno'),
+-- 8 Volt Prime
+('Ninguno', 'Kavasa Prime Kubrow Collar', 'Kubrow Prime Companion'),
+-- 9 Ash Prime
+('Ninguno', 'Targis Prime Armor', 'Ninguno'),
+-- 10 Trinity Prime
+('Ninguno', 'Avia Prime Syandana', 'Ninguno'),
+-- 11 Saryn Prime
+('Ninguno', 'Cycuta Prime Syandana', 'Ninguno'),
+-- 12 Vauban Prime
+('Ninguno', 'Asperitus Prime Syandana', 'Ninguno'),
+-- 13 Nekros Prime
+('Ninguno', 'Uru Prime Syandana', 'Acanthus Prime Armor Set'),
+-- 14 Valkyr Prime
+('Ninguno', 'Furis Prime Syandana', 'Ninguno'),
+-- 15 Banshee Prime
+('Ninguno', 'Soprana Prime Syandana', 'Ninguno'),
+-- 16 Oberon Prime
+('Ninguno', 'Acanthus Prime Syandana', 'Acanthus Prime Armor'),
+-- 17 Hydroid Prime
+('Ninguno', 'Rorqual Prime Syandana', 'Ninguno'),
+-- 18 Mirage Prime
+('Ninguno', 'Abbera Prime Syandana', 'Ninguno'),
+-- 19 Zephyr Prime
+('Ninguno', 'Commodore Prime Suit', 'Ninguno'),
+-- 20 Limbo Prime
+('Ninguno', 'Riftwalker Prime Syandana', 'Ninguno'),
+-- 21 Chroma Prime
+('Ninguno', 'Imugi Prime Syandana', 'Ninguno'),
+-- 22 Mesa Prime
+('Ninguno', 'Cobra Prime Syandana', 'Ninguno'),
+-- 23 Equinox Prime
+('Ninguno', 'Solstice Prime Syandana', 'Ninguno'),
+-- 24 Wukong Prime
+('Ninguno', 'Jindou Prime Syandana', 'Ninguno'),
+-- 25 Atlas Prime
+('Ninguno', 'Tectonic Prime Syandana', 'Ninguno'),
+-- 26 Ivara Prime
+('Ninguno', 'Apavada Prime Syandana', 'Ninguno'),
+-- 27 Titania Prime
+('Ninguno', 'Acanthus Prime Syandana', 'Ninguno'),
+-- 28 Inaros Prime
+('Ninguno', 'Anubis Prime Syandana', 'Ninguno'),
+-- 29 Nezha Prime
+('Ninguno', 'Yaksha Prime Syandana', 'Ninguno'),
+-- 30 Octavia Prime
+('Ninguno', 'Serenade Prime Syandana', 'Ninguno'),
+-- 31 Gara Prime
+('Ninguno', 'Astilla Prime Syandana', 'Ninguno'),
+-- 32 Nidus Prime
+('Ninguno', 'Crinoid Prime Syandana', 'Ninguno'),
+-- 33 Harrow Prime
+('Ninguno', 'Navaros Prime Syandana', 'Ninguno'),
+-- 34 Garuda Prime
+('Ninguno', 'Sakhura Prime Syandana', 'Ninguno'),
+-- 35 Khora Prime
+('Ninguno', 'Kerata Prime Syandana', 'Venari Prime'),
+-- 36 Revenant Prime
+('Ninguno', 'Vania Prime Syandana', 'Ninguno'),
+-- 37 Baruuk Prime
+('Ninguno', 'Kopra Prime Syandana', 'Ninguno'),
+-- 38 Hildryn Prime
+('Ninguno', 'Larkspur Prime Syandana', 'Ninguno'),
+-- 39 Wisp Prime
+('Ninguno', 'Solis Prime Syandana', 'Ninguno'),
+-- 40 Grendel Prime
+('Ninguno', 'Gustav Prime Syandana', 'Ninguno'),
+-- 41 Gauss Prime
+('Blazargaze Prime Ephemera', 'Altra Prime Syandana', 'Gauss Prime Floof'),
+-- 42 Protea Prime
+('Ninguno', 'Velox Prime Syandana', 'Ninguno'),
+-- 43 Sevagoth Prime
+('Ninguno', 'Erebus Prime Syandana', 'Ninguno'),
+-- 44 Xaku Prime
+('Ninguno', 'Artifex Prime Syandana', 'Necra Prime Suit');
+
+INSERT INTO armas_primarias_prime (nombre, elemento, danio) VALUES
+('Ninguno', 'Ninguno', 0), -- Excalibur Prime
+('Latron Prime', 'Impacto', 85), -- Frost Prime
+('Boar Prime', 'Perdigión', 120), -- Mag Prime
+('Sicarus Prime', 'Perforación', 90), -- Ember Prime
+('Boltor Prime', 'Perforación', 90), -- Rhino Prime
+('Ninguno', 'Ninguno', 0), -- Loki Prime
+('Vectis Prime', 'Perforación', 225), -- Nova Prime
+('Ninguno', 'Ninguno', 0), -- Volt Prime
+('Braton Prime', 'Impacto', 70), -- Ash Prime
+('Burston Prime', 'Cortante', 78), -- Trinity Prime
+('Ninguno', 'Ninguno', 0), -- Saryn Prime
+('Paris Prime', 'Perforación', 180), -- Vauban Prime
+('Ninguno', 'Ninguno', 0), -- Nekros Prime
+('Soma Prime', 'Cortante', 120), -- Valkyr Prime
+('Ninguno', 'Ninguno', 0), -- Banshee Prime
+('Tigris Prime', 'Cortante', 1560), -- Oberon Prime
+('Ninguno', 'Ninguno', 0), -- Hydroid Prime
+('Sybaris Prime', 'Cortante', 168), -- Mirage Prime
+('Ninguno', 'Ninguno', 0), -- Zephyr Prime
+('Ninguno', 'Ninguno', 0), -- Limbo Prime
+('Galatine Prime', 'Cortante', 350), -- Chroma Prime
+('Ninguno', 'Ninguno', 0), -- Mesa Prime
+('Ninguno', 'Ninguno', 0), -- Equinox Prime
+('Dakra Prime', 'Cortante', 150), -- Wukong Prime
+('Ninguno', 'Ninguno', 0), -- Atlas Prime
+('Bo Prime', 'Impacto', 140), -- Ivara Prime
+('Ninguno', 'Ninguno', 0), -- Titania Prime
+('Ninguno', 'Ninguno', 0), -- Inaros Prime
+('Ninguno', 'Ninguno', 0), -- Nezha Prime
+('Rubico Prime', 'Perforación', 187), -- Octavia Prime
+('Stradavar Prime', 'Cortante', 180), -- Gara Prime
+('Astilla Prime', 'Perdigones', 480), -- Nidus Prime
+('Ninguno', 'Ninguno', 0), -- Harrow Prime
+('Ninguno', 'Ninguno', 0), -- Garuda Prime
+('Ninguno', 'Ninguno', 0), -- Khora Prime
+('Ninguno', 'Ninguno', 0), -- Revenant Prime
+('Ninguno', 'Ninguno', 0), -- Baruuk Prime
+('Ninguno', 'Ninguno', 0), -- Hildryn Prime
+('Ninguno', 'Ninguno', 0), -- Wisp Prime
+('Glaive Prime', 'Cortante', 120), -- Grendel Prime
+('Ninguno', 'Ninguno', 0), -- Gauss Prime
+('Ninguno', 'Ninguno', 0), -- Protea Prime
+('Ninguno', 'Ninguno', 0), -- Sevagoth Prime
+('Ninguno', 'Ninguno', 0); -- Xaku Prime
+
+INSERT INTO armas_secundarias_prime (nombre, elemento, danio) VALUES
+('Ninguno','Ninguno',0), -- Excalibur Prime
+('Ninguno','Ninguno',0), -- Frost Prime
+('Aklex Prime','Perforación',150), -- Mag Prime
+('Sicarus Prime','Perforación',90), -- Ember Prime
+('Ninguno','Ninguno',0), -- Rhino Prime
+('Ninguno','Ninguno',0), -- Loki Prime
+('Pandero Prime','Cortante',210), -- Nova Prime
+('Akjagara Prime','Cortante',180), -- Volt Prime
+('Lex Prime','Perforación',150), -- Ash Prime
+('Ninguno','Ninguno',0), -- Trinity Prime
+('Spira Prime','Perforación',180), -- Saryn Prime
+('Ninguno','Ninguno',0), -- Vauban Prime
+('Ninguno','Ninguno',0), -- Nekros Prime
+('Ninguno','Ninguno',0), -- Valkyr Prime
+('Magnus Prime','Impacto',160), -- Banshee Prime
+('Ninguno','Ninguno',0), -- Oberon Prime
+('Ninguno','Ninguno',0), -- Hydroid Prime
+('Ninguno','Ninguno',0), -- Mirage Prime
+('Ninguno','Ninguno',0), -- Zephyr Prime
+('Ninguno','Ninguno',0), -- Limbo Prime
+('Ninguno','Ninguno',0), -- Chroma Prime
+('Ninguno','Ninguno',0), -- Mesa Prime
+('Ninguno','Ninguno',0), -- Equinox Prime
+('Ninguno','Ninguno',0), -- Wukong Prime
+('Ninguno','Ninguno',0), -- Atlas Prime
+('Ninguno','Ninguno',0), -- Ivara Prime
+('Ninguno','Ninguno',0), -- Titania Prime
+('Ninguno','Ninguno',0), -- Inaros Prime
+('Ninguno','Ninguno',0), -- Nezha Prime
+('Ninguno','Ninguno',0), -- Octavia Prime
+('Ninguno','Ninguno',0), -- Gara Prime
+('Ninguno','Ninguno',0), -- Nidus Prime
+('Ninguno','Ninguno',0), -- Harrow Prime
+('Ninguno','Ninguno',0), -- Garuda Prime
+('Ninguno','Ninguno',0), -- Khora Prime
+('Ninguno','Ninguno',0), -- Revenant Prime
+('Ninguno','Ninguno',0), -- Baruuk Prime
+('Ninguno','Ninguno',0), -- Hildryn Prime
+('Ninguno','Ninguno',0), -- Wisp Prime
+('Ninguno','Ninguno',0), -- Grendel Prime
+('Ninguno','Ninguno',0), -- Gauss Prime
+('Ninguno','Ninguno',0), -- Protea Prime
+('Ninguno','Ninguno',0), -- Sevagoth Prime
+('Ninguno','Ninguno',0); -- Xaku Prime
+
+INSERT INTO armas_melees_prime (nombre, elemento, danio) VALUES
+('Ninguno','Ninguno',0), -- Excalibur Prime
+('Reaper Prime','Cortante',180), -- Frost Prime
+('Dakra Prime','Cortante',150), -- Mag Prime
+('Glaive Prime','Cortante',120), -- Ember Prime
+('Ankyros Prime','Impacto',130), -- Rhino Prime
+('Orthos Prime','Perforación',155), -- Loki Prime
+('Ninguno','Ninguno',0), -- Nova Prime
+('Bo Prime','Impacto',140), -- Volt Prime
+('Galatine Prime','Cortante',350), -- Ash Prime
+('Fang Prime','Perforación',80), -- Trinity Prime
+('Venka Prime','Cortante',200), -- Saryn Prime
+('Silva & Aegis Prime','Fuego',230), -- Vauban Prime
+('Tipedo Prime','Impacto',190), -- Nekros Prime
+('Dual Kamas Prime','Cortante',150), -- Valkyr Prime
+('Destreza Prime','Perforación',260), -- Banshee Prime
+('Redeemer Prime','Cortante',300), -- Oberon Prime
+('Gram Prime','Cortante',320), -- Hydroid Prime
+('Guandao Prime','Perforación',290), -- Mirage Prime
+('Zakti Prime','Impacto',180), -- Zephyr Prime
+('Ninguno','Ninguno',0), -- Limbo Prime
+('Glaive Prime','Cortante',120), -- Chroma Prime
+('Ninguno','Ninguno',0), -- Mesa Prime
+('Ninguno','Ninguno',0), -- Equinox Prime
+('Ninguno','Ninguno',0), -- Wukong Prime
+('Ninguno','Ninguno',0), -- Atlas Prime
+('Ninguno','Ninguno',0), -- Ivara Prime
+('Ninguno','Ninguno',0), -- Titania Prime
+('Ninguno','Ninguno',0), -- Inaros Prime
+('Ninguno','Ninguno',0), -- Nezha Prime
+('Ninguno','Ninguno',0), -- Octavia Prime
+('Ninguno','Ninguno',0), -- Gara Prime
+('Ninguno','Ninguno',0), -- Nidus Prime
+('Ninguno','Ninguno',0), -- Harrow Prime
+('Ninguno','Ninguno',0), -- Garuda Prime
+('Ninguno','Ninguno',0), -- Khora Prime
+('Ninguno','Ninguno',0), -- Revenant Prime
+('Ninguno','Ninguno',0), -- Baruuk Prime
+('Ninguno','Ninguno',0), -- Hildryn Prime
+('Ninguno','Ninguno',0), -- Wisp Prime
+('Ninguno','Ninguno',0), -- Grendel Prime
+('Ninguno','Ninguno',0), -- Gauss Prime
+('Ninguno','Ninguno',0), -- Protea Prime
+('Ninguno','Ninguno',0), -- Sevagoth Prime
+('Ninguno','Ninguno',0) -- Xaku Prime
