0367fb6d651d1f8a7d6dff13e8747c1a47948be9
Agustin Casaux <95194455+CasauxAgustin@users.noreply.github.com>
2025-11-13 21:29:56 -0300

    Create Crear_Datos_3.sql

---
+++ Scrips SQL/Crear_Datos_3.sql
@@ -0,0 +1,45 @@
+INSERT INTO warframesprime_armasprime (id_warframe, id_arma_primaria, id_arma_secundaria, id_arma_melee) VALUES
+(1, NULL, NULL, NULL),  -- Excalibur Prime
+(2, 2, NULL, 2),        -- Frost Prime (Latron Prime / Reaper Prime)
+(3, 3, 3, 3),           -- Mag Prime (Boar Prime / Aklex Prime / Dakra Prime)
+(4, 4, 4, 4),           -- Ember Prime (Sicarus Prime / Sicarus Prime / Glaive Prime)
+(5, 5, NULL, 5),        -- Rhino Prime (Boltor Prime / Ankyros Prime)
+(6, NULL, NULL, 6),     -- Loki Prime (Orthos Prime)
+(7, 7, 7, NULL),        -- Nova Prime (Vectis Prime / Pandero Prime)
+(8, NULL, 8, 8),        -- Volt Prime (Akjagara Prime / Bo Prime)
+(9, 9, 9, 9),           -- Ash Prime (Braton Prime / Lex Prime / Galatine Prime)
+(10, 10, NULL, 10),     -- Trinity Prime (Burston Prime / Fang Prime)
+(11, NULL, 11, 11),     -- Saryn Prime (Spira Prime / Venka Prime)
+(12, 12, NULL, 12),     -- Vauban Prime (Paris Prime / Silva & Aegis Prime)
+(13, NULL, NULL, 13),   -- Nekros Prime (Ninguno)
+(14, 8, NULL, 14),      -- Valkyr Prime (Soma Prime / Dual Kamas Prime)
+(15, NULL, 15, NULL),   -- Banshee Prime (Magnus Prime)
+(16, 11, NULL, 15),     -- Oberon Prime (Tigris Prime / Redeemer Prime)
+(17, NULL, NULL, 16),   -- Hydroid Prime (Gram Prime)
+(18, 18, NULL, 17),     -- Mirage Prime (Sybaris Prime / Guandao Prime)
+(19, NULL, NULL, 18),   -- Zephyr Prime (Zakti Prime)
+(20, NULL, NULL, NULL), -- Limbo Prime
+(21, 19, NULL, 19),     -- Chroma Prime (Galatine Prime / Glaive Prime)
+(22, NULL, NULL, NULL), -- Mesa Prime
+(23, NULL, NULL, NULL), -- Equinox Prime
+(24, 20, NULL, NULL),   -- Wukong Prime (Dakra Prime)
+(25, NULL, NULL, NULL), -- Atlas Prime
+(26, 21, NULL, NULL),   -- Ivara Prime (Bo Prime)
+(27, NULL, NULL, NULL), -- Titania Prime
+(28, NULL, NULL, NULL), -- Inaros Prime
+(29, NULL, NULL, NULL), -- Nezha Prime
+(30, 22, NULL, NULL),   -- Octavia Prime (Rubico Prime)
+(31, 23, NULL, NULL),   -- Gara Prime (Stradavar Prime)
+(32, 24, NULL, NULL),   -- Nidus Prime (Astilla Prime)
+(33, NULL, NULL, NULL), -- Harrow Prime
+(34, NULL, NULL, NULL), -- Garuda Prime
+(35, NULL, NULL, NULL), -- Khora Prime
+(36, NULL, NULL, NULL), -- Revenant Prime
+(37, NULL, NULL, NULL), -- Baruuk Prime
+(38, NULL, NULL, NULL), -- Hildryn Prime
+(39, NULL, NULL, NULL), -- Wisp Prime
+(40, 25, NULL, 26),     -- Grendel Prime (Glaive Prime)
+(41, NULL, NULL, NULL), -- Gauss Prime
+(42, NULL, NULL, NULL), -- Protea Prime
+(43, NULL, NULL, NULL), -- Sevagoth Prime
+(44, NULL, NULL, NULL); -- Xaku Prime
